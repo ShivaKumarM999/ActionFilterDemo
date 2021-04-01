042583e4be7d0c689ab726306b4addf5fa0b7bc9
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-04-01 17:44:31 +0530

    Add project files.

---
+++ ActionFilterDemo/Controllers/DemoController.cs
@@ -0,0 +1,50 @@
+﻿using ActionFilterDemo.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilterDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        [TrackExecutionTime]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [TrackExecutionTime]
+        public ActionResult Page1()
+        {
+            return View();
+        }
+        [TrackExecutionTime]
+        public ActionResult Page2()
+        {
+            return View();
+        }
+        [TrackExecutionTime]
+        public ActionResult Page3()
+        {
+            return View();
+        }
+        [TrackExecutionTime]
+        public ActionResult Page4()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult Page5()
+        {
+            return PartialView("_PartialDemo");
+        }
+        [ActionName("Shiva")]
+        public ActionResult Page6()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ActionFilterDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilterDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [OutputCache(Duration =50000)]
+        public ActionResult OutCacheTest()
+        {
+            return View();
+        }
+
+        [HandleError(View="CustomError")]
+        public ActionResult CustomErrorDemo()
+        {
+            throw new Exception("Something went wrong!!!");
+        }
+    }
+}
